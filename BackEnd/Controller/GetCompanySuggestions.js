31dd6ceffe5c8f8536c7a532f5231677eb22342e
Sami Shibli <sshibli@scu.edu>
2024-11-09 12:50:04 -0800

    search bar functionality

---
+++ BackEnd/Controller/GetCompanySuggestions.js
@@ -0,0 +1,24 @@
+/** @format */
+
+import { pool } from "../Database/Database.js";
+
+export const getCompanySuggestions = async (request, response) => {
+	const { companyName } = request.body;
+
+	try {
+		const databaseQuery = `
+        SELECT company_name, revenue
+        FROM all_company_data
+        WHERE company_name ILIKE $1
+        ORDER BY revenue DESC
+        LIMIT 10;`;
+
+		const companyNameCaseInsensitive = [`${companyName}%`];
+		const returnedResult = await pool.query(databaseQuery, companyNameCaseInsensitive);
+		const companySuggestions = returnedResult.rows.map((row) => row.company_name);
+		response.json(companySuggestions);
+	} catch (error) {
+		console.error("GetCompanySuggestions.js: Error fetching Suggestions", error);
+		response.status(500).send("Backend Server Error");
+	}
+};
--- BackEnd/Routes/Routes.js
+++ BackEnd/Routes/Routes.js
@@ -2,6 +2,13 @@
 
 import express from "express";
 
+import { getCompanySuggestions } from "../Controller/GetCompanySuggestions.js";
+import { getCompanyCIK } from "../Controller/GetCompanyCIK.js";
+
 const expressRouter = express.Router();
+
+expressRouter.post("/suggestCompanies", getCompanySuggestions);
+
+expressRouter.post("/getCompanyCIK", getCompanyCIK);
 
 export default expressRouter;
