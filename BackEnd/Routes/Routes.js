8ef51e8c8fe385578e62ed4f9c7f687a92c4e8d8
Calvin Lim <calvinwlim@yahoo.com>
2024-11-13 13:44:48 -0800

    New Routes

--- BackEnd/Routes/Routes.js
+++ BackEnd/Routes/Routes.js
@@ -2,13 +2,19 @@
 
 import express from "express";
 
-import { getCompanySuggestions } from "../Controller/GetCompanySuggestions.js";
-import { getCompanyCIK } from "../Controller/GetCompanyCIK.js";
+import { getCompanySuggestions } from "../DatabaseMethods/GetCompanySuggestions.js";
+import { getCompanyCIK } from "../DatabaseMethods/GetCompanyCIK.js";
+import { fetchFormSection } from "../DatabaseMethods/GetAnnualReportData.js";
+import { fetchFilingSummary } from "../DatabaseMethods/GetFilingSummary.js";
 
 const expressRouter = express.Router();
 
 expressRouter.post("/suggestCompanies", getCompanySuggestions);
 
 expressRouter.post("/getCompanyCIK", getCompanyCIK);
+
+expressRouter.post("/getCompanyFormSection", fetchFormSection);
+
+expressRouter.post("/getFilingSummary", fetchFilingSummary);
 
 export default expressRouter;
