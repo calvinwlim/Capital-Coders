f10faf0ef3594e4208071cebf043cdd453f0a758
Calvin Lim <calvinwlim@yahoo.com>
2024-11-15 12:38:49 -0800

    Firebase utils

---
+++ FrontEnd/Firebase/firebase.utils.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB6QJ3uLunEXntFx8pzVV73JGCIUIxhmW4",
+  authDomain: "capital-coders.firebaseapp.com",
+  projectId: "capital-coders",
+  storageBucket: "capital-coders.firebasestorage.app",
+  messagingSenderId: "332284931614",
+  appId: "1:332284931614:web:b3f9fd35a7a44a8016480f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
