2ce368df02d954441acdde117c9f8e866a5a1c48
KallanFraser <117051126+KallanFraser@users.noreply.github.com>
2024-11-15 13:39:14 -0800

    Update main.jsx
    Working annual page

--- FrontEnd/main.jsx
+++ FrontEnd/main.jsx
@@ -1,26 +1,23 @@
+/** @format */
+
 import React, { StrictMode } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 
-import './main.css';
 import HomePage from "./HomePage/HomePage";
-import LoginPage from "./LoginPage/LoginPage";
 import FormExplorerPage from "./FormExplorerPage/FormExplorerPage";
+import AnnualReportAnalysisPage from "./AnnualReportAnalysisPage/AnnualReportAnalysisPage";
 
 createRoot(document.getElementById("root")).render(
-	<StrictMode>
-		<Router>
-			<Routes>
-				<Route path="/" element={<HomePage />} />
-				<Route path="/FormExplorerPage/:cik" element={<FormExplorerPage />} />
-				<Route path="/login" element={<LoginPage />} />
-				{/* <Route
-					path="/AnnualReportAnalysis/:cik/:accessionNumber/:ticker/:date/:form"
-					element={<AnnualReportAnalysisPage />}
-				/> */}
-			</Routes>
-		</Router>
-	</StrictMode>
+  <StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/FormExplorerPage/:cik" element={<FormExplorerPage />} />
+        <Route path="/AnnualReportAnalysis/:cik/:accessionNumber/:ticker/:date/:form" element={<AnnualReportAnalysisPage />} />
+      </Routes>
+    </Router>
+  </StrictMode>
 );
 
 //navigate(`/displayReports/${cik}/${accessionNumber}/${ticker}/${formattedDate}/${form}`);
